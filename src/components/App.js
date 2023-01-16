cad28121a7a9e24562ab51f710179a8529b1f93d
isaac <isaactonyloi@gmail.com>
2023-01-16 09:48:11 +0300

    props destructuring

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import MovieCard from "./MovieCard";
 
 function App() {
+  const posterSrc =  "https://images.unsplash.com/photo-1529781912305-50e0d2217bef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80";
   const title = "Mad Max";
-  const posterURL =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
+  //const genres = genresArr.join(", ");
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      {/* posterSrc is omitted, so the default value will be used instead */}
+      <MovieCard src={posterSrc} title={title} genres={genresArr} />
     </div>
   );
 }
